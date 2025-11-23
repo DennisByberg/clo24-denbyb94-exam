d82103c67a846882365314c51eb06ac398844095
dennisbyberg <dennis.byberg@hotmail.com>
2025-11-23 10:22:45 +0100

    add CORS middleware for frontend communication #108

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.config import settings
 
 app = FastAPI(
     title=settings.app_name,
     version=settings.app_version,
     description=settings.app_description,
+)
+
+# Allows frontend to communicate with backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
