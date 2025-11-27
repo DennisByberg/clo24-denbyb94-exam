f3765a87abc9c186891dd837b84c65d7d4545ed0
dennisbyberg <dennis.byberg@hotmail.com>
2025-11-28 00:18:48 +0100

    feat: add theme configuration using Mantine for consistent styling #90

---
+++ frontend/src/theme/theme.ts
@@ -0,0 +1,31 @@
+import { createTheme } from '@mantine/core';
+
+export const theme = createTheme({
+  primaryColor: 'dark',
+  colors: {
+    dark: [
+      '#d5d7e0', // 0
+      '#acaebf', // 1
+      '#8c8fa3', // 2
+      '#666980', // 3
+      '#4d4f66', // 4
+      '#34354a', // 5
+      '#2b2c3d', // 6
+      '#16161C', // 7  (Background)
+      '#0e0e1a', // 8
+      '#05050a', // 9
+    ],
+    yellow: [
+      '#fff9db', // 0
+      '#fff3bf', // 1
+      '#ffec99', // 2  (Text)
+      '#ffe066', // 3
+      '#ffd43b', // 4
+      '#fcc419', // 5
+      '#fab005', // 6
+      '#f59f00', // 7
+      '#f08c00', // 8
+      '#e67700', // 9
+    ],
+  },
+});
