e85c7348ea1d8b60bfb9274520880b004ca6ad37
dennisbyberg <dennis.byberg@hotmail.com>
2025-12-01 22:25:41 +0100

    feat: update scripts to use dynamic storage account name for image operations #90

--- infrastructure/scripts/delete-restaurant-images.sh
+++ infrastructure/scripts/delete-restaurant-images.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Deletes all restaurant images from Azure Blob Storage
+# Usage: STORAGE_ACCOUNT_NAME=essgroupstorage ./delete-restaurant-images.sh
+
+STORAGE_ACCOUNT_NAME="${STORAGE_ACCOUNT_NAME:-essgroupstorage}"
 
 az storage blob delete-batch \
-  --account-name essgroupstorage \
+  --account-name "$STORAGE_ACCOUNT_NAME" \
   --source restaurant-images \
   --pattern "restaurant-*.jpeg" \
   --auth-mode login
--- infrastructure/scripts/upload-restaurant-images.sh
+++ infrastructure/scripts/upload-restaurant-images.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Uploads all restaurant images to Azure Blob Storage
+# Usage: STORAGE_ACCOUNT_NAME=essgroupstorage ./upload-restaurant-images.sh
+
+STORAGE_ACCOUNT_NAME="${STORAGE_ACCOUNT_NAME:-essgroupstorage}"
 
 az storage blob upload-batch \
-  --account-name essgroupstorage \
+  --account-name "$STORAGE_ACCOUNT_NAME" \
   --destination restaurant-images \
   --source "../images/restaurant" \
   --pattern "restaurant-*.jpeg" \
